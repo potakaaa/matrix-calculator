0d799ba21efbe3ec92ec325116deea5b5c9a4d76
potakaaa <helbirog@gmail.com>
2024-06-20 14:32:00 +0800

    Matrix size on GUI problem solved

--- qt files/matrix-calculator/matrixcalcu.cpp
+++ qt files/matrix-calculator/matrixcalcu.cpp
@@ -9,18 +9,49 @@ matrixCalcu::matrixCalcu(QWidget *parent)
 
     setWindowTitle("Matrix Calculator");
 
-    QGridLayout *layout = new QGridLayout(ui->frame_6);
+
 
     // Placeholder for matrix A size
-    int matrixA_rows = 3; int matrixA_cols = 3; int matrixA_size = matrixA_rows*matrixA_cols;
+    int matrixA_rows = 4; int matrixA_cols = 3; int matrixA_size = matrixA_rows*matrixA_cols;
 
     int rowNum = 0; int colNum = 0; int i = 0;
-    std::string name;
 
     QLineEdit* lineEdit_matrixA[matrixA_size];
+    for (int x = 0; x < matrixA_size; x++) {
+        lineEdit_matrixA[x] = new QLineEdit();
+    }
 
-    QLineEdit* sample = new QLineEdit(ui->frame_6);
+    ui->gridLayout->addWidget(ui->label_enterMatrixA_entry, 0, 0, 1, matrixA_cols);
+    ui->gridLayout->addWidget(ui->label_matrixA_size_entry, matrixA_rows+1, 0, 1, matrixA_cols);
+
+    for (rowNum = 1; rowNum < matrixA_rows+1; rowNum++) {
+        for(colNum = 0; colNum < matrixA_cols; colNum++) {
+            ui->gridLayout->addWidget(lineEdit_matrixA[i], rowNum, colNum);
+            i++;
+        }
+
+    }
+
+
     /**
+    ui->gridLayout->addWidget(ui->label_enterMatrixA_entry, 0, 0, 1, matrixA_cols);
+    ui->gridLayout->addWidget(ui->label_matrixA_size_entry, matrixA_rows+1, 0, 1, matrixA_cols);
+
+
+
+    ui->gridLayout->addWidget(lineEdit_matrixA[i], rowNum, colNum);
+    ui->gridLayout->addWidget(lineEdit_matrixA[i+1], rowNum, colNum+1);
+
+
+
+    /**
+*     for (rowNum = 1; rowNum <= matrixA_rows; rowNum++) {
+        for(colNum = 0; colNum <= matrixA_cols; colNum++) {
+            ui->gridLayout->addWidget(lineEdit_matrixA[i], rowNum, colNum);
+            i++;
+        }
+
+    }
     for (rowNum = 0; rowNum <= matrixA_rows; rowNum++) {
         for(colNum = 0; colNum <= matrixA_cols; colNum++) {
             layout->addWidget(lineEdit_matrixA[i], rowNum, colNum);
